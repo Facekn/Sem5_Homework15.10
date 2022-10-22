6be9994260259f5758f16a9eedbccc13cc16085b
Ilya Grigorev <facekn110388@gmail.com>
2022-10-22 12:20:58 +0300

    Копирование массива

---
+++ Sem5_Homework3/Program.cs
@@ -0,0 +1,30 @@
+﻿//Написать программу копирования массива
+
+void FillArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)    
+        array[i] = new Random().Next(-10,10);       
+}
+
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write ($"|{array[i]}| ");     
+}
+
+void CopyingArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+       array[i] = array[i];  
+}
+
+Console.Write("Введите длинну массива: ");
+int length = int.Parse(Console.ReadLine()?? "0");
+int [] array = new int [length];
+FillArray (array);
+Console.WriteLine("Изначальный массив:");
+PrintArray (array);
+Console.WriteLine();
+CopyingArray(array);
+Console.WriteLine ("Скопированный массив:");
+PrintArray(array);
